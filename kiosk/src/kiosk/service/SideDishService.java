448f8343638b67f3f60b440de180f4ef30f508d0
Lee jieon <giun27@naver.com>
2019-03-22 17:33:52 +0900

    Side

--- kiosk/src/kiosk/service/SideDishService.java
+++ kiosk/src/kiosk/service/SideDishService.java
@@ -1,11 +1,13 @@
 package kiosk.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import kiosk.dao.KioskDAO;
 
 public class SideDishService {
 		
-		private KioskDAO dao;
+		/*private KioskDAO dao;
 		public SideDishService() {
 			this.dao = new  KioskDAO();
 		}
@@ -18,9 +20,9 @@ public class SideDishService {
 				System.out.println("         사이드 선택");
 				System.out.println("==================================");
 				System.out.println("번호     이름     가격");
-				Item[] list = this.dao.listItem("sideDish");
+				List<Item> item = this.dao.listItem("sideDish");
 				int count = 1;
-				for (Item i : list) {
+				for (Item i : item) {
 					if (i != null) {
 						System.out.printf("%3d  %3s   %3s%n"
 								, count, i.getName()
@@ -32,9 +34,14 @@ public class SideDishService {
 				System.out.print("선택:");
 				int m = sc.nextInt();
 				sc.nextLine();
+				List<Item> temp = new ArrayList<Item>();
+				temp.add(item.get(m-1));
 				System.out.print("수량:");
 				int m_ = sc.nextInt();
 				sc.nextLine();
+				
+				SubOrder s1 = new SubOrder(temp, m_);
+				this.dao.add(s1);
 				System.out.println("장바구니에 추가되었습니다.");
 
 				if (m == 0)
@@ -43,7 +50,8 @@ public class SideDishService {
 			
 				}
 
-			}		
+			}
+			*/		
 		}
 
 
