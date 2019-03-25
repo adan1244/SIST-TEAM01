3cc350201ea16ad602f20c14ded7851f0c70ff76
JHRla <wookkya2@gmail.com>
2019-03-25 15:32:42 +0900

    CartService

--- kiosk/src/kiosk/service/CartService.java
+++ kiosk/src/kiosk/service/CartService.java
@@ -10,6 +10,8 @@ public class CartService {
 	private KioskDAO dao;
 	private Order order = new Order();
 	private Membership membership = new Membership();
+	int total;
+	int usePoint;
 	
 	//장바구니(결제) 메뉴
 	public void menu03(Scanner sc) {
@@ -19,9 +21,12 @@ public class CartService {
 			System.out.println("==================================");
 			System.out.println("          장바구니 (결제)"         );
 			System.out.println("==================================");
-			//dao.listOrder();
+			order = dao.getCart();
 			System.out.println("----------------------------------");
-			//가격출력 (총금액)
+			for(int i =0; i<order.getItem().size(); ++i) {
+				total += order.getItem().get(i).getPrice() *  order.getItem().get(i).getCount(); 
+			}
+			System.out.printf("총 금액 : %d", total);
 			System.out.println("----------------------------------");
 			System.out.println("1. 결제하기");
 			System.out.println("2. 삭제하기");
@@ -57,29 +62,7 @@ public class CartService {
 					System.out.println("잘못된 입력입니다.");
 				}
 			} else if(input == 2) {
-				System.out.println("삭제할 메뉴의 번호를 입력하세요.");
-				System.out.print("선택 : ");
-				int orderId = sc.nextInt();
-				sc.nextLine();
-				
-				System.out.println("삭제할 수량을 입력하세요.");
-				System.out.print("선택 : ");
-				int orderNum = sc.nextInt();
-				sc.nextLine();
-				
-				System.out.println("정말 삭제하시겠습니까? (YES : 1 / NO : 0)");
-				System.out.print("선택 : ");
-				int input2 = sc.nextInt();
-				sc.nextLine();
-				
-				if(input2 == 1) {
-					//dao.deleteFromCart();
-					System.out.println("선택한 메뉴가 장바구니에서 삭제되었습니다.");
-				}else if(input2 == 0){
-					System.out.println("취소되었습니다.");
-				}else {
-					System.out.println("잘못된 입력입니다.");
-				}
+				this.deleteOrder(sc);
 			} else {
 				System.out.println("잘못된 입력입니다.");
 			}
@@ -97,7 +80,7 @@ public class CartService {
 			if(cardNum.length() != 19) {
 				System.out.println("유효하지 않은 카드 번호입니다.");
 			}else {
-				//System.out.printf("총 %d원 결제되었습니다. %n", 총금액);
+				System.out.printf("총 %d원 결제되었습니다. %n", total);
 				int point = 0;
 				this.bill(point);
 				break;
@@ -122,9 +105,8 @@ public class CartService {
 					if(phoneNum.length() != 13) {
 						System.out.println("유효하지 않은 핸드폰 번호입니다.");
 					}else {
-						//System.out.printf("총 %d원 결제되었습니다. %n", 총금액);
-						//int point = 총금액 * 0.1 + membership.getPoint();
-						int point = 0;
+						System.out.printf("총 %d원 결제되었습니다. %n", total);
+						int point = (int)(total * 0.1) + membership.getPoint();
 						System.out.printf("%d 포인트 적립", point);
 						
 						membership.setPoint(point);
@@ -152,14 +134,14 @@ public class CartService {
 			}else {
 				System.out.printf("적립된 포인트 %d원", point);
 				System.out.println("사용할 포인트 : ");
-				int usePoint = sc.nextInt();
+				usePoint = sc.nextInt();
 				sc.nextLine();
 				
 				if(usePoint > point) {
 					System.out.println("적립된 포인트 범위를 벗어났습니다.");
 				}else {
 					membership.setPoint(point - usePoint);
-					//System.out.printf("남은 결제 금액 : %d", 총 금액 - usepoint);
+					System.out.printf("남은 결제 금액 : %d", total - usePoint);
 					break;
 				}
 			}
@@ -173,7 +155,7 @@ public class CartService {
 			if(cardNum.length() != 19) {
 				System.out.println("유효하지 않은 카드 번호입니다.");
 			}else {
-				//System.out.printf("총 %d원 결제되었습니다. %n", 총금액);
+				System.out.printf("총 %d원 결제되었습니다. %n", total);
 				this.bill(membership.getPoint());
 				break;
 			}
@@ -184,15 +166,42 @@ public class CartService {
 	//영수증 출력
 	public void bill(int point) {
 		System.out.println("==============영수증===============");
-		//System.out.printf("날    짜 : %s", order.getDate());
+		System.out.printf("날    짜 : %s", order.getDate());
 		System.out.println("==================================");
-		//System.out.println(order.getItem());
+		System.out.println(order.getItem());
 		System.out.println("==================================");
-		//System.out.printf("총    액 : %d", 총 금액);
-		//System.out.println("포인트 사용 : %d");
+		System.out.printf("총    액 : %d", total);
+		System.out.printf("포인트 사용 : -%d", usePoint);
 		System.out.println("==================================");
-		//System.out.printf("결제금액 : %d", 총 금액);
+		System.out.printf("결제금액 : %d", total - usePoint);
 		System.out.println("==================================");
 		System.out.printf("적 립 금 : %d", point);
+	}
+	
+	//주문 삭제 메소드
+	public void deleteOrder(Scanner sc) {
+		System.out.println("삭제할 메뉴의 번호를 입력하세요.");
+		System.out.print("선택 : ");
+		int orderId = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.println("삭제할 수량을 입력하세요.");
+		System.out.print("선택 : ");
+		int count = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.println("정말 삭제하시겠습니까? (YES : 1 / NO : 0)");
+		System.out.print("선택 : ");
+		int input2 = sc.nextInt();
+		sc.nextLine();
+		
+		if(input2 == 1) {
+			dao.deleteFromCart(orderId, count);
+			System.out.println("선택한 메뉴가 장바구니에서 삭제되었습니다.");
+		}else if(input2 == 0){
+			System.out.println("취소되었습니다.");
+		}else {
+			System.out.println("잘못된 입력입니다.");
+		}
 	}
 }
