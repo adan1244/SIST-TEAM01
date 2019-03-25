5267b5c6428648fca10b1b56f4bc3a832d2a31ff
JHRla <wookkya2@gmail.com>
2019-03-25 16:44:28 +0900

    update

--- kiosk/src/kiosk/main/Main.java
+++ kiosk/src/kiosk/main/Main.java
@@ -12,12 +12,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		KioskDAO dao = new KioskDAO();
-		
+
 		SandwitchService sandwitch = new SandwitchService(dao);
-		//SideDishService sideDish = new SideDishService(dao);	
-		CartService cart = new CartService(dao);	
-		AdminService admin = new AdminService(dao);	
-        Scanner sc = new Scanner(System.in);
+		// SideDishService sideDish = new SideDishService(dao);
+		CartService cart = new CartService(dao);
+		AdminService admin = new AdminService(dao);
+		Scanner sc = new Scanner(System.in);
 
 		while (true) {
 
@@ -33,23 +33,20 @@ public class Main {
 			int m = sc.nextInt();
 			sc.nextLine();
 
-			if (m == 0) break;
+			if (m == 0)
+				break;
 
 			switch (m) {
 			case 1: sandwitch.menu1(sc); break;
-			//case 2: sideDish.menu2(sc); break;
+			// case 2: sideDish.menu2(sc); break;
 			case 3: cart.menu3(sc); break;
 			case 99: admin.menu99(sc); break;
 			}
-			
+
 		}
-		   
+
 		sc.close();
 		System.out.println("프로그램 종료");
 	}
 
-	}
- 
-
-
-
+}
