81bc21a77a084ac0dd93308260241008afb8299a
JHRla <wookkya2@gmail.com>
2019-03-27 15:26:16 +0900

    오류 수정

--- kiosk/src/kiosk/service/SideDishService.java
+++ kiosk/src/kiosk/service/SideDishService.java
@@ -26,16 +26,15 @@ public class SideDishService {
 	public void menu2(Scanner sc) {
 		while (true) {
 			List<Item> item = this.dao.listItem("SI");
-			List<Item> temp = new ArrayList<Item>();
 			int count = 1;
-			
+
 			System.out.println();
 			System.out.println("뒤로 가기 = 0");
 			System.out.println("==================================");
 			System.out.println("           사이드 선택");
 			System.out.println("==================================");
 			System.out.println("번호          이름            가격");
-			
+
 			for (Item i : item) {
 				if (i != null) {
 					System.out.printf("%4d%12s%15s%n", count, i.getName(), i.getPrice());
@@ -50,28 +49,36 @@ public class SideDishService {
 			
 			if (m == 0) return;
 
-			temp.add(item.get(m - 1));
-			
-			System.out.print("수량:");
-			int m_ = sc.nextInt();
-			sc.nextLine();
-			
-			if (m_ != 0) {
-				SubOrder s1 = new SubOrder(temp, m_);
-				this.dao.addToCart(s1);
-				Order order = dao.getCart();
-				
-				System.out.println("장바구니에 추가되었습니다.");
-				System.out.println("");
-				System.out.println("== 현재 장바구니에 추가된 메뉴 ===");
-				
-				for (int i = 0; i < order.getItem().size(); ++i) {
-					System.out.printf("%d %s%n ", i + 1, order.getItem().get(i));
-				}
+			if (m >= count) {
+				System.out.println("없는 메뉴 입니다");
 
-				System.out.println("----------------------------------");
+			} else {
+				List<Item> temp = new ArrayList<Item>();
+				temp.add(item.get(m - 1));
+				
+				System.out.print("수량:");
+				int m_ = sc.nextInt();
+				sc.nextLine();
+				
+				if (m_ != 0) {
+					SubOrder s1 = new SubOrder(temp, m_);
+					this.dao.addToCart(s1);
+					Order order = dao.getCart();
+					
+					System.out.println("장바구니에 추가되었습니다.");
+					System.out.println("");
+					System.out.println("== 현재 장바구니에 추가된 메뉴 ===");
+					System.out.printf(" %d %s%n ", 1, order.getItem().get(0));
+					
+					for (int i = 1; i < order.getItem().size(); ++i) {
+						System.out.printf("%d %s%n ", i + 1, order.getItem().get(i));
+					}
+
+					System.out.println("----------------------------------");
+					return;
+				}
 			}
-			return;
+
 		}
 
 	}
