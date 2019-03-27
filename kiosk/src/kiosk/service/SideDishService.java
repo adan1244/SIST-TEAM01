b0be25ca9b28cd8b2b0bacb6ea4df0641701c8c4
JHRla <wookkya2@gmail.com>
2019-03-27 14:56:48 +0900

    삭제 메소드 수정

--- kiosk/src/kiosk/service/SideDishService.java
+++ kiosk/src/kiosk/service/SideDishService.java
@@ -8,7 +8,6 @@ import kiosk.dao.KioskDAO;
 import kiosk.domain.Item;
 import kiosk.domain.Order;
 import kiosk.domain.SubOrder;
-import kiosk.main.Main;
 
 public class SideDishService {
 
@@ -26,49 +25,53 @@ public class SideDishService {
 	// 사이드메뉴 주문
 	public void menu2(Scanner sc) {
 		while (true) {
-
+			List<Item> item = this.dao.listItem("SI");
+			List<Item> temp = new ArrayList<Item>();
+			int count = 1;
+			
 			System.out.println();
 			System.out.println("뒤로 가기 = 0");
 			System.out.println("==================================");
 			System.out.println("           사이드 선택");
 			System.out.println("==================================");
 			System.out.println("번호          이름            가격");
-			List<Item> item = this.dao.listItem("SI");
-			int count = 1;
+			
 			for (Item i : item) {
 				if (i != null) {
 					System.out.printf("%4d%12s%15s%n", count, i.getName(), i.getPrice());
 					++count;
 				}
 			}
+			
 			System.out.println("----------------------------------");
 			System.out.print("선택:");
 			int m = sc.nextInt();
 			sc.nextLine();
-			if (m == 0)
-				break;
-			List<Item> temp = new ArrayList<Item>();
+			
+			if (m == 0) return;
+
 			temp.add(item.get(m - 1));
+			
 			System.out.print("수량:");
 			int m_ = sc.nextInt();
 			sc.nextLine();
+			
+			if (m_ != 0) {
+				SubOrder s1 = new SubOrder(temp, m_);
+				this.dao.addToCart(s1);
+				Order order = dao.getCart();
+				
+				System.out.println("장바구니에 추가되었습니다.");
+				System.out.println("");
+				System.out.println("== 현재 장바구니에 추가된 메뉴 ===");
+				
+				for (int i = 0; i < order.getItem().size(); ++i) {
+					System.out.printf("%d %s%n ", i + 1, order.getItem().get(i));
+				}
 
-			SubOrder s1 = new SubOrder(temp, m_);
-			this.dao.addToCart(s1);
-			System.out.println("장바구니에 추가되었습니다.");
-			System.out.println("");
-			System.out.println("== 현재 장바구니에 추가된 메뉴 ===");
-			Order order = dao.getCart();
-			for (int i = 0; i < order.getItem().size(); ++i) {
-				System.out.printf("%d %s%n ", i + 1, order.getItem().get(i));
+				System.out.println("----------------------------------");
 			}
-
-			System.out.println("----------------------------------");
-
-			if (m_ == 0)
-				Main.main(null);
-			break;
-
+			return;
 		}
 
 	}
